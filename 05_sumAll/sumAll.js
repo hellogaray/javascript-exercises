c2991f58b30cb39d9109dfc1a2b5a76d48e7aabe
Leo <137473678+hellogaray@users.noreply.github.com>
2024-03-25 20:47:48 +0900

    Excercices 4-5  completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    total = 0;
+    if (start < 0 || start < 0 || typeof(start) != "number" || typeof(end) != "number") {
+        return "ERROR"
+    };
+    if (start > end) {
+        let temp = start;
+        start = end;
+        end = temp;
+    };
+    for (let count = start; count <= end; count++) {
+        total += count
+    }
+	return total
 };
 
 // Do not edit below this line
