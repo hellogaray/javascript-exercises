3066a1fa62de09e2a244467780e5792580b06869
Leo <137473678+hellogaray@users.noreply.github.com>
2024-03-25 20:49:34 +0900

    Working on ex

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return "ERROR"
+    }
+    stringToPrint = ''
+    for (let count = 0; count < num; count++) {
+        stringToPrint += string
+    };
+    return stringToPrint;
 };
 
 // Do not edit below this line
