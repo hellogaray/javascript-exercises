19ed399007b64fb7db74e4052cc869e8e70a490a
Leo <137473678+hellogaray@users.noreply.github.com>
2024-03-25 19:54:03 +0900

    Excercices 1-3 completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    newString = '';
+    for (let position = string.length -1; position >= 0; position --) {
+        newString += string[position];
+    };
+    return newString;
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
+"hello"
